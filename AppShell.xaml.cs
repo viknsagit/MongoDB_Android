230150113b3fb2e2999c890e47ee4c8e8fa7bf44
viknsagit <viknsagitarius@gmail.com>
2023-10-10 23:53:58 +0500

    add custom flyout and routes

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -6,5 +6,17 @@
         {
             InitializeComponent();
         }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("///MainPage");
+            Shell.Current.FlyoutIsPresented = false;
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("///SavedConnections");
+            Shell.Current.FlyoutIsPresented = false;
+        }
     }
 }
--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -9,12 +9,15 @@
 
         private void ImageButton_Clicked(object sender, EventArgs e)
         {
+            Shell.Current.FlyoutBehavior = FlyoutBehavior.Locked;
+            Shell.Current.FlyoutBehavior = FlyoutBehavior.Flyout;
             Shell.Current.FlyoutIsPresented = true;
         }
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("///SavedConnections");
+            Shell.Current.FlyoutIsPresented = false;
         }
     }
 }
